2b003d28c257c71d26a53571e83b28c75f5c0a3d
kitechin <kitechin@qq.com>
2022-11-01 20:57:48 +0800

    add update.sh

---
+++ update.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cp ~/.gitconfig ~/MiscConfig/
+cp ~/.zsh_history ~/MiscConfig/
+cp ~/.zshrc ~/MiscConfig/
+cp ~/.config/alacritty/alacritty.yml ~/MiscConfig/
+cp ~/.config/nvim/coc-settings.json ~/MiscConfig/
+cp ~/.config/i3/config ~/MiscConfig/
+cp ~/.config/ranger/rc.conf ~/MiscConfig/
+cp ~/.config/ranger/rifle.conf ~/MiscConfig/
